ae3d00870c97aa8f8fb917ee83e3d39f4fbc663d
Vladislove <xxww44@mail.ru>
2023-01-16 18:50:30 +0700

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2*number2)
+{
+    Console.WriteLine("da");
+}
+else
+{
+    Console.WriteLine("net");
+}
+
