3ab68ea73cb6d18d26fa969b1d6f4493b0603d71
Vladislove <xxww44@mail.ru>
2023-01-17 18:21:57 +0700

    Lesson 1\Task02

---
+++ Homework/lesson 1/Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberc = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (numbera>numberb)
+{
+    max = numbera;
+    Console.Write(max);
+}
+else if (numberb>numberc)
+{
+    max = numberb;
+    Console.Write(max);
+}
+else if (numberc>numbera)
+{
+    max = numberc;
+    Console.Write(max);
+}
