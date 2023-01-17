7257f85b081ce36d3fd2f399860f838b5ffc7988
Vladislove <xxww44@mail.ru>
2023-01-17 18:30:52 +0700

    Lesson 1\Task03

---
+++ Homework/lesson 1/Task03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2==0)
+{
+    Console.Write("Da"); 
+}
+else
+{
+    Console.Write("net");
+}
