390be92e9dc47395f44fbe8c16ea5b10f789e3fb
Vladislove <xxww44@mail.ru>
2023-01-23 16:28:47 +0700

    Создал доп папки seminar , lecture . перенёс файлы в папку seminar

--- Task03/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = -number;
-while (count <= number)
-{
-    Console.Write(count + " ");
-    count++;
-}
