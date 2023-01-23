8258bc4206643de69f20a27811892606d62cee2a
Vladislove <xxww44@mail.ru>
2023-01-23 18:52:17 +0700

    lecture task03

---
+++ lecture/lecture 2/Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿//
+int[] array = { 12, 23, 34, 45, 75, 86, 97, 18 };
+
+int n = array.Length; // длина массива
+int find = 4;
+
+int index = 0;
+
+while (index < n) // Если index < n
+{
+    if (array[index] == find) // Тогда array c [index] == find
+    {
+        Console.WriteLine(index); // выводим index числа
+        break; // прерывание  
+    }
+    index++;  //index = index + 1 (Если нет то увеличиваем индекс на 1)    
+}
