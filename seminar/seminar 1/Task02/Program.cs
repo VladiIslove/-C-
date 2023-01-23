379fd9cee01c8efe8970087346961655cc3e0bbf
Vladislove <xxww44@mail.ru>
2023-01-23 16:28:03 +0700

    Создал доп папки seminar , lecture . перенёс файлы в папку seminar

---
+++ seminar/seminar 1/Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.Write("Сегодня Понидельник");
+}
+else if (number ==2)
+{
+    Console.Write("Сегодня Вторник");
+}
+else if (number == 3)
+{
+    Console.Write("Сегодня Среда");
+}
+else if (number == 4)
+{
+    Console.Write("Сегодня Четверг");
+}
+else if (number == 5)
+{
+    Console.Write("Сегодня Пятница");
+}
+else if (number == 6)
+{
+    Console.Write("Сегодня Суббота");
+}
+else if (number == 7)
+{
+    Console.Write("Сегодня Воскресенье");   
+}
+    
---
+++ seminar/seminar 1/Task04/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трёхзначное число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 1000)
+    Console.Write($"Последняя цифра в числе {number} равна {number % 10}");
+else
+    Console.Write("Вы ввели не трёхзначное число");
+
