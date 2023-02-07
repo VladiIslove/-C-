7a4c8bfb63b4aeec35397ef6066796aa4ea46ce9
Vladislove <xxww44@mail.ru>
2023-02-07 13:35:24 +0700

    Homework lesson 2

---
+++ Homework/lesson 2/Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет. Выполнить с помощью числовых операций 
+//(целочисленное деление, остаток от деления).
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    int firdNumber = number % 10;
+    Console.WriteLine($"Третья цифра => {firdNumber}");
+}
+else if (number < -99)
+{
+    while (number < -999)
+    {
+        number = number / 10;
+    }
+
+    int firdNumber = number % 10 * -1;
+    Console.WriteLine($"Третья цифра => {firdNumber}");
+}
+else
+{
+    Console.WriteLine("Третьего числа нет.");
+}
---
+++ Homework/lesson 2/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7) Console.WriteLine("Такого дня недели нет!");
+else
+{
+    Console.WriteLine(SquareNumber(number) ? "да" : "нет");
+}
+
+bool SquareNumber(int num1)
+{
+    return num1 % 6 == 0 || num1 % 7 == 0;
+}
